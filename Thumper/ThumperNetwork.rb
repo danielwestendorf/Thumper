5d1a850a28fb00d0636c76a74e620de3dd2d5a48
Daniel Westendorf <dwestendorf@scottusa.com>
2011-04-07 07:31:48 -0600

    removed files not used, removed border on tb buttons

--- Thumper/ThumperNetwork.rb
+++
@@ -1,9 +0,0 @@
-#
-#  NetworkDelegate.rb
-#  Thumper
-#
-#  Created by Daniel Westendorf on 4/2/11.
-#  Copyright 2011 Scott USA. All rights reserved.
-#
-
-
--- ThumperIndexDelegate.rb
+++
@@ -1,9 +0,0 @@
-#
-#  ThumperIndexDelegate.rb
-#  Thumper
-#
-#  Created by Daniel Westendorf on 4/3/11.
-#  Copyright 2011 Scott USA. All rights reserved.
-#
-
-
