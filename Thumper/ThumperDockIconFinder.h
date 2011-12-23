a2ca38b9dc726c8a90fe196203d612e1c3e4601b
Daniel Westendorf <daniel@prowestech.com>
2011-12-22 17:33:34 -0700

    updated icons

---
+++ Thumper/ThumperDockIconFinder.h
@@ -0,0 +1,13 @@
+//
+//  ThumperDockIconFinder.h
+//  Thumper
+//
+//  Created by Daniel Westendorf on 12/22/11.
+//  Copyright 2011 Daniel Westendorf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThumperDockIconFinder : NSObject
+
+@end
---
+++ Thumper/ThumperDockIconFinder.m
@@ -0,0 +1,23 @@
+//
+//  ThumperDockIconFinder.m
+//  Thumper
+//
+//  Created by Daniel Westendorf on 12/22/11.
+//  Copyright 2011 Daniel Westendorf. All rights reserved.
+//
+
+#import "ThumperDockIconFinder.h"
+
+@implementation ThumperDockIconFinder
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
